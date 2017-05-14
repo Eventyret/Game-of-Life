34042deaf21a97167bd14d54b9e0f5dc2227c297
Simen Daehlin <simen.dehlin@gmail.com>
2017-05-14 11:27:05 +0100

    minor fixes

--- game_of_life.py
+++ game_of_life.py
@@ -1,14 +1,14 @@
 import sys
-
+ 
 import pygame
-
+ 
 pygame.init()
-colums, rows = 50, 50
+columns, rows = 50, 50
 cell_size = 10
-size = width, height = colums * cell_size, rows * cell_size
-screen ? pygame.display.set_mode(size)
-
+size = width, height = columns * cell_size, rows * cell_size
+screen = pygame.display.set_mode(size)
+ 
 while True:
-    for event in pygam.event.get():
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
